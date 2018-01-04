102bd21c8e55ec6d08746e2b643f902bb3bade4d
Joseph Tomlinson and Kimberly Bordon <joseph+kimberly@>
2018-01-04 11:58:08 -0800

    start person model

---
+++ TravelBlog/Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TravelBlog.Models;
+using TravelBlog.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TravelBlog.Controllers
+{
+    public class PersonController : Controller
+    {
+
+        private TravelBlogContext db = new TravelBlogContext();
+
+        public IActionResult Index()
+        {
+           Experience experience = db.Experiences.FirstOrDefault(l => l.Id == id);
+           ICollection<Person> people = db.People.Where(e => e.ExperienceId == id).ToList();
+
+           PersonListModel model = new PersonListModel(experience, people);
+           return View(model);
+        }
+    }
+}
---
+++ TravelBlog/ViewModels/PersonListModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using TravelBlog.Models;
+
+namespace TravelBlog.ViewModels
+{
+    public class PersonListModel
+    {
+        public Experience Experience { get; set; } 
+        public virtual ICollection<Person> People { get; set; }
+
+        public PersonListModel(Experience experience, ICollection<Person> people)
+        {
+            Experience = experience;
+            People = people;
+        }
+    }
+}
